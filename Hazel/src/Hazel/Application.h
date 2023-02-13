ecca2ff155a9a77611cd5722939b397bb1c9fa1b
liy-dn <liy-dn@glodon.com>
2023-02-13 20:12:02 +0800

    Added vertex array support.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,7 @@
 
 #include "Hazel/Renderer/Buffer.h"
 #include <Hazel/Renderer/Shader.h>
+#include "Hazel/Renderer/VertexArray.h"
 namespace Hazel {
   class HAZEL_API Application
   {
@@ -32,10 +33,13 @@ namespace Hazel {
 
     LayerStack m_LayerStack;
 
-    unsigned int m_VertexArray;
-    std::unique_ptr<Shader> m_Shader;
-    std::unique_ptr<VertexBuffer> m_VertexBuffer;
-    std::unique_ptr<IndexBuffer> m_IndexBuffer;
+    std::shared_ptr <VertexArray> m_VertexArray;
+    std::shared_ptr<Shader> m_Shader;
+
+   // std::shared_ptr<VertexBuffer> m_VertexBuffer;
+   // std::shared_ptr<IndexBuffer> m_IndexBuffer;
+    std::shared_ptr<Shader> m_BlueShader;
+    std::shared_ptr<VertexArray> m_SquareVA;
   private:
     static Application* s_Instance;
   };
---
+++ Hazel/src/Hazel/Renderer/VertexArray.cpp
@@ -0,0 +1,16 @@
+#include "hzpch.h"
+#include "VertexArray.h"
+
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLVertexArray.h"
+namespace Hazel {
+  Hazel::VertexArray* VertexArray::Create()
+  {
+    switch (Renderer::GetAPI())
+    {
+    case RendererAPI::None:HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+    case RendererAPI::OpenGL:return new OpenGLVertexArray();
+    
+    }
+  }
+}
