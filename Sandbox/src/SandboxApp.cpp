19072c2210d1d65c0f14186bbf9254f5955dd5c3
liy-dn <liy-dn@glodon.com>
2023-02-16 20:57:57 +0800

    Added transforms to Renderer.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -46,10 +46,10 @@ public:
     m_SquareVA.reset(VertexArray::Create());
 
     float squareVertices[3 * 4] = {
-      -0.75f, -0.75f, 0.0f,
-       0.75f, -0.75f, 0.0f,
-       0.75f,  0.75f, 0.0f,
-      -0.75f,  0.75f, 0.0f
+         -0.5f, -0.5f, 0.0f,
+          0.5f, -0.5f, 0.0f,
+          0.5f,  0.5f, 0.0f,
+         -0.5f,  0.5f, 0.0f
     };
     std::shared_ptr<VertexBuffer> squareVB;
     squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
@@ -72,13 +72,14 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
       uniform mat4 u_ViewProjection;
+      uniform mat4 u_Transform;
 			out vec3 v_Position;
 			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position =u_ViewProjection* vec4(a_Position, 1.0);	
+				gl_Position =u_ViewProjection* u_Transform *vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -102,13 +103,13 @@ public:
 			
 			layout(location = 0) in vec3 a_Position;
 
-   uniform mat4 u_ViewProjection;
-
+      uniform mat4 u_ViewProjection;
+      uniform mat4 u_Transform;
 			out vec3 v_Position;
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position =u_ViewProjection* vec4(a_Position, 1.0);	
+				gl_Position =u_ViewProjection* u_Transform *vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -152,7 +153,19 @@ public:
    HZ_INFO("{0},{1},  {2}",m_CameraPosition.x,m_CameraPosition.y,  m_CameraRotation);
     Renderer::BeginScene(m_Camera);
 
-    Renderer::Submit(m_BlueShader, m_SquareVA);
+    glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+    for (int y = 0; y < 20; y++)
+    {
+      for (int x = 0; x < 20; x++)
+      {
+        glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+        Hazel::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+      }
+    }
+
+   // Renderer::Submit(m_BlueShader, m_SquareVA);
     Renderer::Submit(m_Shader, m_VertexArray);
 
     Renderer::EndScene();
