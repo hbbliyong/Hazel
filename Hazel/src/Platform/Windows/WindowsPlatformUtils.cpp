e93bb01afafc730f83a99d037cd4c59ea85c8491
liy-dn <liy-dn@glodon.com>
2024-03-30 19:25:07 +0800

    Fixed some issue

--- Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -12,11 +12,16 @@ namespace Hazel {
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
+		CHAR currentDir[256] = { 0 };
 		ZeroMemory(&ofn, sizeof(OPENFILENAME));
 		ofn.lStructSize = sizeof(OPENFILENAME);
 		ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
 		ofn.lpstrFile = szFile;
 		ofn.nMaxFile = sizeof(szFile);
+		if (GetCurrentDirectoryA(256,currentDir))
+		{
+			ofn.lpstrInitialDir = currentDir;
+		}
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
 		ofn.lpstrDefExt = strchr(filter, '\0') + 1;
@@ -33,14 +38,17 @@ namespace Hazel {
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
+		CHAR currentDir[256] = { 0 };
 		ZeroMemory(&ofn, sizeof(OPENFILENAME));
 		ofn.lStructSize = sizeof(OPENFILENAME);
 		ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
 		ofn.lpstrFile = szFile;
 		ofn.nMaxFile = sizeof(szFile);
+		if (GetCurrentDirectoryA(256, currentDir))
+			ofn.lpstrInitialDir = currentDir;
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
-		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
 		ofn.lpstrDefExt = strchr(filter, '\0') + 1;
 		if (GetSaveFileNameA(&ofn) == TRUE)
 		{
