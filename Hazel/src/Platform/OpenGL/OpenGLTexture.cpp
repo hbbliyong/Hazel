52258e631499fe4b3ca548ecf4e4140cc0ce3b5b
liy-dn <liy-dn@glodon.com>
2024-04-28 08:53:01 +0800

    - Added IsLoaded to Texture to see if texture data was actually loaded - We now catch YAML::ParserException (although this isn't a reliable way of catching invalid YAML files) - Added .hazel extension checking to EditorLayer::OpenScene

--- Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -3,77 +3,91 @@
 
 #include <stb_image.h>
 
-namespace Hazel {
+namespace Hazel
+{
 
-  OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
-    :m_Width(width), m_Height(height)
-  {
-    m_InternalFormat = GL_RGBA8;
-    m_DataFormat = GL_RGBA;
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		:m_Width(width), m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
 
-    glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-    glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
 
-    glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  }
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
 
-  OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
-    : m_Path(path)
-  {
-    int width, height, channels;
-    stbi_set_flip_vertically_on_load(1);
-    stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
-    HZ_CORE_ASSERT(data, "Failed to load image!");
-    m_Width = width;
-    m_Height = height;
+	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
+		: m_Path(path)
+	{
+		HZ_PROFILE_FUNCTION();
 
-    GLenum internalFormat = 0, dataFormat = 0;
-    if (channels == 4) {
-      internalFormat = GL_RGBA8;
-      dataFormat = GL_RGBA;
-    }
-    else if(channels == 3) 
-    {
-      internalFormat = GL_RGB8;
-      dataFormat = GL_RGB;
-    }
-    m_InternalFormat = internalFormat;
-    m_DataFormat = dataFormat;
+		int width, height, channels;
+		stbi_set_flip_vertically_on_load(1);
+		stbi_uc* data = nullptr;
+		{
+			HZ_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
+			stbi_load(path.c_str(), &width, &height, &channels, 0);
+		}
 
-    HZ_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!");
+		if (data)
+		{
+			m_IsLoaded = true;
 
-    glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-    glTextureStorage2D(m_RendererID, 1, internalFormat, m_Width, m_Height);
+			m_Width = width;
+			m_Height = height;
 
-    glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			GLenum internalFormat = 0, dataFormat = 0;
+			if (channels == 4)
+			{
+				internalFormat = GL_RGBA8;
+				dataFormat = GL_RGBA;
+			}
+			else if (channels == 3)
+			{
+				internalFormat = GL_RGB8;
+				dataFormat = GL_RGB;
+			}
 
-    glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			m_InternalFormat = internalFormat;
+			m_DataFormat = dataFormat;
 
-    glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, dataFormat, GL_UNSIGNED_BYTE, data);
+			HZ_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!");
 
-    stbi_image_free(data);
-  }
+			glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+			glTextureStorage2D(m_RendererID, 1, internalFormat, m_Width, m_Height);
 
-  OpenGLTexture2D::~OpenGLTexture2D()
-  {
-    glDeleteTextures(1, &m_RendererID);
-  }
+			glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  void OpenGLTexture2D::SetData(void* data, uint32_t size)
-  {
-    uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
-    HZ_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
-    glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
-  }
+			glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-  void OpenGLTexture2D::Bind(uint32_t slot /*= 0*/) const
-  {
-    glBindTextureUnit(slot, m_RendererID);
-  }
+			glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, dataFormat, GL_UNSIGNED_BYTE, data);
+
+			stbi_image_free(data);
+		}
+	}
+
+	OpenGLTexture2D::~OpenGLTexture2D()
+	{
+		glDeleteTextures(1, &m_RendererID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		HZ_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
+	}
+
+	void OpenGLTexture2D::Bind(uint32_t slot /*= 0*/) const
+	{
+		glBindTextureUnit(slot, m_RendererID);
+	}
 }
--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -333,7 +333,15 @@ namespace Hazel {
 					{
 						const wchar_t* path=(const wchar_t*)payload->Data;
 						std::filesystem::path texturePath=std::filesystem::path(g_AssetPath)/path;
-						component.Texture=Texture2D::Create(texturePath.string());
+						Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+						if (texture->IsLoaded())
+						{
+							component.Texture= texture;
+						}
+						else
+						{
+							HZ_WARN("Could not load texture {0}", texturePath.filename().string());
+						}
 					}
 					ImGui::EndDragDropTarget();
 				}
